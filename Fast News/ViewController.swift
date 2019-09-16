8915293123ce70b91c2e9eb1aaf4b9763f291657
Lucas Moreton <lucas.moreton@dextra-sw.com>
2019-09-16 01:31:35 -0300

    Initial Commit

---
+++ Fast News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fast News
+//
+//  Created by Lucas Moreton on 16/09/19.
+//  Copyright © 2019 Lucas Moreton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
